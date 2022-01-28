3b971d4d4d368da0a381e6dfaf7777527b8d0606
soccercreater <96107026+soccercreater@users.noreply.github.com>
2022-01-28 10:20:40 +0000

    8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,31 +25,27 @@ class UsersController < ApplicationController
      @book = Book.new
    end
 
-  def create
+   def create
      @user = User.new(user_params)
      if @user.save
        flash[:notice] = 'Book was successfully created'
       redirect_to user_path(@user.id)
-      else
+     else
       render :new
-      end
-    end
-
-
-
-    @book.user_id = current_user.id
-   if @book.save
-     flash[:notice] = 'Book was successfully created'
-    redirect_to user_path(user.id)
-   else
-     @book = Book.new
-     render :index
+     end
+        @book.user_id = current_user.id
+     if @book.save
+         flash[:notice] = 'Book was successfully created'
+        redirect_to user_path(user.id)
+     else
+         @book = Book.new
+         render :index
+     end
    end
-  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction)
   end
-
+end
